c18edbd9bb706dff4a201a8e986fff3e6ea5f88f
Carlos Daniel <92409234+CarlosDanielAponte@users.noreply.github.com>
2024-01-09 16:00:24 -0600

    Add files via upload

---
+++ PHP/resultado.php
@@ -0,0 +1,19 @@
+<?php 
+include 'head2.html';
+
+//Recibiendo datos desde el index.php usando el metodo post
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+
+//Mostrando los datos recibidos
+echo "<p>Mi nombre es: $nombre</p>";
+echo "<p>Mi edad es: $edad</p>";
+echo "<p>Mi correo es: $correo</p>";
+echo "<p>Mi contraseña es: $contrasena</p>";
+echo "<a href= 'Index.php'>Back</a>";//Boton para regresar
+
+
+
+?>
